c47d61beea915c18ebfe2f5454e1ac3a478f9d53
rkomiyama <ryukomiyama@gmail.com>
2019-07-25 15:44:05 -0400

    Setting up basic layout

---
+++ src/components/ProfileBioContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout fill-height>
+    <v-card class="ma-auto">
+      <v-card-title>Name</v-card-title>
+      <v-card-text>Stuff</v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'ProfileBioContainer'
+}
+</script>
