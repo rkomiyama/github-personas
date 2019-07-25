dbcef62f3e1d562546cc66eef0a74d39107b1b64
rkomiyama <ryukomiyama@gmail.com>
2019-07-25 15:49:03 -0400

    Remove jQuery package

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import jquery from 'jquery';
-
-window.$ = jquery;
-window.JQuery = jquery;
 
 Vue.config.productionTip = false
 
